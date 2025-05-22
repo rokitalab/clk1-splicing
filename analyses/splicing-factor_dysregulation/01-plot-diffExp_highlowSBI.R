a8220a0757f95d8c872aec698c421219e7f87364
pj-sullivan <patricia.sullivan199@gmail.com>
2025-05-22 19:02:07 +0000

    cluster6 group for SFD-01

--- analyses/splicing-factor_dysregulation/01-plot-diffExp_highlowSBI.R
+++ analyses/splicing-factor_dysregulation/01-plot-diffExp_highlowSBI.R
@@ -74,12 +74,20 @@ other_hgg_bs_id <- clin_tab %>%
   filter(plot_group == "Other high-grade glioma") %>%
   pull(Kids_First_Biospecimen_ID)
 
+# create cluster subset
+clust_file <- file.path(root_dir, "analyses/sample-psi-clustering/results/sample-cluster-metadata-top-5000-events-stranded.tsv")
+clust_df <- read_tsv(clust_file)
+
+cluster6_bs_id <- clust_df %>%
+  filter(cluster == 6) %>%
+  pull(sample_id)
+
 # read in files, join palette with sbi file
 sbi_coding_df  <-  readr::read_tsv(sbi_coding_file, comment = "#") %>%
   dplyr::rename(Kids_First_Biospecimen_ID = Sample) %>%
   filter(Kids_First_Biospecimen_ID %in% clin_tab$Kids_First_Biospecimen_ID)
 
-bs_list <- list("all_hgg" = hgg_bs_id, "dmg" = dmg_bs_id, "other_hgg" = other_hgg_bs_id)
+bs_list <- list("all_hgg" = hgg_bs_id, "dmg" = dmg_bs_id, "other_hgg" = other_hgg_bs_id, "cluster6" = cluster6_bs_id)
 names <- names(bs_list)
 
 for (each in names) {
