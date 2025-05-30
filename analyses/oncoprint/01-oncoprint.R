43593d981ff8566d7e5cce133d6827819ab587de
rjcorb <rcorbett@childrensnational.org>
2025-05-30 13:24:26 -0400

    add oncoprint by cluster, run cluster mutation fisher test

--- analyses/oncoprint/01-oncoprint.R
+++ analyses/oncoprint/01-oncoprint.R
@@ -13,6 +13,7 @@ suppressPackageStartupMessages({
   library(data.table)
   library(ComplexHeatmap)
   library(circlize)
+  library(gtools)
   
 })
 
@@ -48,12 +49,12 @@ cnv_file <- file.path(data_dir, "consensus_wgs_plus_freec_wxs_plus_freec_tumor_o
 psi_exp_file <- file.path(root_dir, "analyses", "CLK1-splicing_correlations", "results", "hgg-dmg-clk-srsf-expression-phosphorylation.tsv")
 fus_file <- file.path(data_dir, "fusion-putative-oncogenic.tsv")
 diff_psi_file <- file.path(root_dir, "analyses", "splicing_events_functional_sites", "results", "splice_events.diff.SE.txt")
+cluster_file <- file.path(root_dir, "analyses", 
+                          "sample-psi-clustering", "results", 
+                          "sample-cluster-metadata-top-5000-events-stranded.tsv")
 
 ## color for barplot
 source(file.path(input_dir, "mutation-colors.R"))
-
-## output file
-plot_out <- file.path(plots_dir,"oncoprint.pdf")
 
 # read in files
 histologies_df <- read_tsv(clin_file, guess_max = 100000) %>%
@@ -76,12 +77,16 @@ goi <- read_csv(goi_file) %>%
   unique() %>%
   c("CLK1")
 
+cluster_df <- read_tsv(cluster_file)
+
 indep_rna_df <- vroom(indep_rna_file) %>% 
   dplyr::filter(cohort == 'PBTA') %>%
   # get match id for DNA samples
   left_join(histologies_df[,c("Kids_First_Biospecimen_ID", "match_id", "plot_group", "RNA_library")]) %>%
   filter(plot_group %in% c("DIPG or DMG", "Other high-grade glioma"),
-         RNA_library %in% c("stranded", "poly-A stranded"))
+         RNA_library %in% c("stranded", "poly-A stranded")) %>%
+  left_join(cluster_df %>% dplyr::select(sample_id, cluster),
+            by = c("Kids_First_Biospecimen_ID" = "sample_id"))
 
 matched_dna_samples <- histologies_df %>%
   filter(experimental_strategy %in% c("WGS", "WXS", "Targeted Panel"),
@@ -107,14 +112,14 @@ splice_df <-  read_tsv(psi_exp_file) %>%
                 `Total CLK1` = as.numeric(scale(`CLK1`))) 
 
 high_clk1_psi <- splice_df %>%
-  arrange(-IncLevel1) %>%
-  slice(1:length(low_clk1_psi)) %>%
+  dplyr::arrange(-IncLevel1) %>%
+  dplyr::slice(1:length(low_clk1_psi)) %>%
   pull(Kids_First_Biospecimen_ID)
 
 splice_df_anno <- splice_df %>%
   mutate(clk1_status = case_when(Kids_First_Biospecimen_ID %in% low_clk1_psi ~ "Low",
-                              Kids_First_Biospecimen_ID %in% high_clk1_psi ~ "High",
-                              TRUE ~ NA_character_))
+                                 Kids_First_Biospecimen_ID %in% high_clk1_psi ~ "High",
+                                 TRUE ~ NA_character_))
 
 # read in cnv file and reformat to add to maf
 cnv_df <- read_tsv(cnv_file) %>%
@@ -228,9 +233,10 @@ histologies_df_sorted <- splice_df_anno %>%
   left_join(histologies_df, by = "match_id", relationship = "many-to-many") %>%
   select(match_id, plot_group, cancer_predisposition, reported_gender, molecular_subtype, CNS_region, `CLK1-201 (Exon4) PSI`,
          `CLK1-201`, `Total CLK1`, clk1_status) %>%
+  left_join(indep_rna_df %>% dplyr::select(match_id, cluster)) %>%
   unique() %>%
   group_by(match_id, plot_group, cancer_predisposition, reported_gender, molecular_subtype, `CLK1-201 (Exon4) PSI`,
-            `CLK1-201`, `Total CLK1`, clk1_status) %>%
+           `CLK1-201`, `Total CLK1`, clk1_status, cluster) %>%
   summarise(CNS_region = str_c(unique(na.omit(CNS_region)), collapse = ","),
             CLK1_PSI = mean(`CLK1-201 (Exon4) PSI`),
             .groups = "drop") %>%
@@ -249,23 +255,26 @@ histologies_df_sorted <- splice_df_anno %>%
 
 histologies_df_sorted2 <- histologies_df_sorted %>%
   select(reported_gender,  cancer_predisposition, plot_group, molecular_subtype, CNS_region, tmb_status, 
-         CLK1_PSI, `CLK1-201`, `Total CLK1`) %>%
+         CLK1_PSI, `CLK1-201`, `Total CLK1`, cluster) %>%
   dplyr::rename("Gender"=reported_gender,
                 "Histology" = plot_group,
                 "Predisposition" = cancer_predisposition,
                 "Molecular Subtype"=molecular_subtype,
                 "CNS Region"=CNS_region, 
                 "Mutation Status"=tmb_status,
-               # "CLK1 status" = clk1_status,
+                # "CLK1 status" = clk1_status,
                 "CLK1 Ex4 PSI"= CLK1_PSI,
                 "CLK1-201" =`CLK1-201`,
-                "Total CLK1 RNA" = `Total CLK1`) 
+                "Total CLK1 RNA" = `Total CLK1`,
+                "Cluster" = cluster) %>%
+  dplyr::mutate(Cluster = factor(Cluster)) %>%
+  dplyr::mutate(Cluster = fct_relevel(Cluster, mixedsort(as.character(unique(Cluster)))))
 
 # write out metadata
 histologies_df_sorted2 %>%
   rownames_to_column(var = "match_id") %>%
   left_join(unique(histologies_df[,c("match_id", "Kids_First_Participant_ID")])) %>%
-  write_tsv(file.path(results_dir, "oncoprint_sample_metadata.tsv"))
+write_tsv(file.path(results_dir, "oncoprint_sample_metadata.tsv"))
 
 loc_cols <- c("#88CCEE", "#CC6677", "#DDCC77", "#117733", "#332288", "#AA4499", "#44AA99", "#882255", "#6699CC")
 
@@ -279,8 +288,8 @@ ha = HeatmapAnnotation(name = "annotation",
                                       "Female" = "pink",
                                       "Unknown" = "whitesmoke"),
                          "Histology" = c("DIPG or DMG" = "#ff40d9",
-
-                                            "Other high-grade glioma" = "#ffccf5"),
+                                         
+                                         "Other high-grade glioma" = "#ffccf5"),
                          "Predisposition" = c("LFS" = "red",
                                               "NF-1" = "black",
                                               "Other" = "grey"),
@@ -301,51 +310,59 @@ ha = HeatmapAnnotation(name = "annotation",
                                                "Unknown" = "whitesmoke"),
                          "CLK1 Ex4 PSI" = colorRamp2(c(-4, 0, 2), c("darkblue","white", "red")),
                          "CLK1-201" = colorRamp2(c(-3, 0, 3), c("darkblue", "white",  "red")),
-                         "Total CLK1 RNA" = colorRamp2(c(-3, 0, 3), c("darkblue", "white",  "red"))
-                         ),
+                         "Total CLK1 RNA" = colorRamp2(c(-3, 0, 3), c("darkblue", "white",  "red")),
+                         "Cluster" = c("1" = "#B2DF8A",
+                                       "2" = "#E31A1C",
+                                       "4" = "#A6CEE3",
+                                       "5" = "#FB9A99",
+                                       "6" = "#FDBF6F",
+                                       "8" = "#FFFF99",
+                                       "10" = "#B15928")
+                       ),
                        annotation_name_side = "right", 
                        annotation_name_gp = gpar(fontsize = 9),
                        na_col = "whitesmoke")
 
 
 col = colors
-df = histologies_df_sorted2
 
 gene_matrix_sorted <- gene_matrix %>%
-  select(all_of(rownames(df)))
+  select(all_of(rownames(histologies_df_sorted2)))
 
 # global option to increase space between heatmap and annotations
 ht_opt$ROW_ANNO_PADDING = unit(1.25, "cm")
 
+alter_fun = list(
+  background = function(x, y, w, h) grid.rect(x, y, w, h, gp = gpar(fill = "whitesmoke",col="whitesmoke")),
+  Missense_Mutation = function(x, y, w, h) grid.rect(x, y, w*0.85, h*0.85, gp = gpar(fill = unname(col["Missense_Mutation"]),col = NA)),
+  Nonsense_Mutation = function(x, y, w, h) grid.rect(x, y, w*0.85, h*0.85, gp = gpar(fill = unname(col["Nonsense_Mutation"]),col = NA)),
+  Frame_Shift_Del = function(x, y, w, h) grid.rect(x, y, w*0.85, h*0.85, gp = gpar(fill = unname(col["Frame_Shift_Del"]), col = NA)),
+  Frame_Shift_Ins = function(x, y, w, h) grid.rect(x, y, w*0.85, h*0.85, gp = gpar(fill = unname(col["Frame_Shift_Ins"]), col = NA)),
+  Splice_Site = function(x, y, w, h) grid.rect(x, y, w*0.85, h*0.85, gp = gpar(fill = unname(col["Splice_Site"]), col = NA)),
+  Translation_Start_Site = function(x, y, w, h) grid.rect(x, y, w*0.85, h*0.85, gp = gpar(fill = unname(col["Translation_Start_Site"]), col = NA)),
+  Nonstop_Mutation = function(x, y, w, h) grid.rect(x, y, w*0.85, h*0.85, gp = gpar(fill = unname(col["Nonstop_Mutation"]),col = NA)),
+  In_Frame_Del = function(x, y, w, h) grid.rect(x, y, w*0.85, h*0.85, gp = gpar(fill = unname(col["In_Frame_Del"]),col = NA)),
+  In_Frame_Ins = function(x, y, w, h) grid.rect(x, y, w*0.85, h*0.85, gp = gpar(fill = unname(col["In_Frame_Ins"]), col = NA)),
+  Stop_Codon_Ins = function(x, y, w, h) grid.rect(x, y, w*0.85, h*0.85, gp = gpar(fill = unname(col["Stop_Codon_Ins"]), col = NA)),
+  Start_Codon_Del = function(x, y, w, h) grid.rect(x, y, w*0.85, h*0.85, gp = gpar(fill = unname(col["Start_Codon_Del"]), col = NA)),
+  Fusion = function(x, y, w, h) grid.rect(x, y, w*0.85, h*0.85, gp = gpar(fill = unname(col["Fusion"]),col = NA)),
+  Multi_Hit_Fusion  = function(x, y, w, h) grid.rect(x, y, w*0.85, h*0.85, gp = gpar(fill = unname(col["Multi_Hit_Fusion"]),col = NA)),
+  Multi_Hit = function(x, y, w, h) grid.rect(x, y, w*0.85, h*0.85, gp = gpar(fill = unname(col["Multi_Hit"]), col = NA)),
+  Del = function(x, y, w, h) grid.rect(x, y, w*0.85, h*0.85, gp = gpar(fill = unname(col["Del"]), col = NA)),
+  Amp = function(x, y, w, h) grid.rect(x, y, w*0.85, h*0.85, gp = gpar(fill = unname(col["Amp"]), col = NA)),
+  Loss = function(x, y, w, h) grid.rect(x, y, w*0.85, h*0.85, gp = gpar(fill = unname(col["Loss"]), col = NA)))
+
 plot_oncoprint <- oncoPrint(gene_matrix_sorted[1:25,], get_type = function(x) strsplit(x, ",")[[1]],
                             column_names_gp = gpar(fontsize = 9), show_column_names = F,
-                            alter_fun = list(
-                              background = function(x, y, w, h) grid.rect(x, y, w, h, gp = gpar(fill = "whitesmoke",col="whitesmoke")),
-                              Missense_Mutation = function(x, y, w, h) grid.rect(x, y, w*0.85, h*0.85, gp = gpar(fill = unname(col["Missense_Mutation"]),col = NA)),
-                              Nonsense_Mutation = function(x, y, w, h) grid.rect(x, y, w*0.85, h*0.85, gp = gpar(fill = unname(col["Nonsense_Mutation"]),col = NA)),
-                              Frame_Shift_Del = function(x, y, w, h) grid.rect(x, y, w*0.85, h*0.85, gp = gpar(fill = unname(col["Frame_Shift_Del"]), col = NA)),
-                              Frame_Shift_Ins = function(x, y, w, h) grid.rect(x, y, w*0.85, h*0.85, gp = gpar(fill = unname(col["Frame_Shift_Ins"]), col = NA)),
-                              Splice_Site = function(x, y, w, h) grid.rect(x, y, w*0.85, h*0.85, gp = gpar(fill = unname(col["Splice_Site"]), col = NA)),
-                              Translation_Start_Site = function(x, y, w, h) grid.rect(x, y, w*0.85, h*0.85, gp = gpar(fill = unname(col["Translation_Start_Site"]), col = NA)),
-                              Nonstop_Mutation = function(x, y, w, h) grid.rect(x, y, w*0.85, h*0.85, gp = gpar(fill = unname(col["Nonstop_Mutation"]),col = NA)),
-                              In_Frame_Del = function(x, y, w, h) grid.rect(x, y, w*0.85, h*0.85, gp = gpar(fill = unname(col["In_Frame_Del"]),col = NA)),
-                              In_Frame_Ins = function(x, y, w, h) grid.rect(x, y, w*0.85, h*0.85, gp = gpar(fill = unname(col["In_Frame_Ins"]), col = NA)),
-                              Stop_Codon_Ins = function(x, y, w, h) grid.rect(x, y, w*0.85, h*0.85, gp = gpar(fill = unname(col["Stop_Codon_Ins"]), col = NA)),
-                              Start_Codon_Del = function(x, y, w, h) grid.rect(x, y, w*0.85, h*0.85, gp = gpar(fill = unname(col["Start_Codon_Del"]), col = NA)),
-                              Fusion = function(x, y, w, h) grid.rect(x, y, w*0.85, h*0.85, gp = gpar(fill = unname(col["Fusion"]),col = NA)),
-                              Multi_Hit_Fusion  = function(x, y, w, h) grid.rect(x, y, w*0.85, h*0.85, gp = gpar(fill = unname(col["Multi_Hit_Fusion"]),col = NA)),
-                              Multi_Hit = function(x, y, w, h) grid.rect(x, y, w*0.85, h*0.85, gp = gpar(fill = unname(col["Multi_Hit"]), col = NA)),
-                              Del = function(x, y, w, h) grid.rect(x, y, w*0.85, h*0.85, gp = gpar(fill = unname(col["Del"]), col = NA)),
-                              Amp = function(x, y, w, h) grid.rect(x, y, w*0.85, h*0.85, gp = gpar(fill = unname(col["Amp"]), col = NA)),
-                              Loss = function(x, y, w, h) grid.rect(x, y, w*0.85, h*0.85, gp = gpar(fill = unname(col["Loss"]), col = NA))),
+                            alter_fun = alter_fun,
                             col = col,
                             top_annotation = ha,
                             alter_fun_is_vectorized = TRUE,
-                            #bottom_annotation = ha1,
                             column_order =  colnames(gene_matrix_sorted))
 
 # Save plot as PDF
-pdf(plot_out, width = 15, height = 7)
+pdf(file.path(plots_dir,"oncoprint-clk1-psi.pdf"),
+    width = 15, height = 7)
 plot_oncoprint
 dev.off()
 
@@ -357,8 +374,12 @@ ids_clk1 <- histologies_df_sorted %>%
 total_high <- nrow(ids_clk1)/2
 total_low <- nrow(ids_clk1)/2
 
-
 alteration_counts <- collapse_snv_dat %>%
+  distinct(match_id, Hugo_Symbol, .keep_all = TRUE) %>%
+  dplyr::mutate(count = case_when(
+    count > 0 ~ 1,
+    TRUE ~ 0
+  )) %>%
   full_join(ids_clk1) %>%
   filter(!is.na(clk1_status)) %>%
   ## group by junction and calculate means
@@ -385,3 +406,80 @@ alteration_counts <- collapse_snv_dat %>%
   ungroup() %>%
   arrange(P_Value) %>%
   write_tsv(file.path(results_dir, "clk1_high_low_mutation_counts.tsv"))
+
+# sort samples by histology and cluster and regenerate oncoprint
+cluster_order <- histologies_df_sorted2 %>%
+  arrange(Histology, Cluster) %>%
+  rownames_to_column("match_id") %>%
+  pull(match_id)
+
+plot_oncoprint_cluster <- oncoPrint(gene_matrix_sorted[1:30,], get_type = function(x) strsplit(x, ",")[[1]],
+                            column_names_gp = gpar(fontsize = 9), show_column_names = F,
+                            alter_fun = alter_fun,
+                            col = col,
+                            top_annotation = ha,
+                            alter_fun_is_vectorized = TRUE,
+                            column_order =  cluster_order)
+
+# Save plot as PDF
+pdf(file.path(plots_dir,"oncoprint-hist-cluster.pdf"),
+    width = 15, height = 7)
+plot_oncoprint_cluster
+dev.off()
+
+# assess for non-random distribution of mutations across HGG/DMG clusters
+ids_cluster <- histologies_df_sorted %>%
+  rownames_to_column(var = "match_id") %>%
+  select(match_id, cluster, plot_group)
+
+cluster_totals <- table(histologies_df_sorted$cluster)
+
+alteration_counts_cluster <- collapse_snv_dat %>%
+  distinct(match_id, Hugo_Symbol, .keep_all = TRUE) %>%
+  # convert counts to 0 (no mutation) or 1 (at least one mutation)
+  dplyr::mutate(count = case_when(
+    count > 0 ~ 1,
+    TRUE ~ 0
+  )) %>%
+  full_join(ids_cluster) %>%
+  filter(!is.na(cluster)) %>%
+  ## group by junction and calculate means
+  dplyr::select(Hugo_Symbol, cluster) %>%
+  group_by(Hugo_Symbol, cluster) %>%
+  count() %>%
+  ungroup() %>%
+  # Spread to wide format to get separate columns for "High" and "Low"
+  pivot_wider(names_from = cluster, values_from = n, values_fill = list(n = 0)) %>%
+  dplyr::select(Hugo_Symbol, `1`, `2`, `4`, `5`, `6`, `8`, `10`) %>%
+  rowwise() %>%
+  mutate(
+    Fisher_Test = list(
+      fisher.test(
+        # create matrix of pts with and without mutations by cluster
+        matrix(
+          c(`1`, `2`, `4`, `5`, `6`, `8`, `10`, 
+            cluster_totals["1"] - `1`,
+            cluster_totals["2"] - `2`,
+            cluster_totals["4"] - `4`,
+            cluster_totals["5"] - `5`,
+            cluster_totals["6"] - `6`,
+            cluster_totals["8"] - `8`,
+            cluster_totals["10"] - `10`),    
+          nrow = 7
+        )
+      )
+    ),
+    P_Value = Fisher_Test$p.value
+  ) %>%
+  select(-Fisher_Test) %>%
+  ungroup() %>%
+  arrange(P_Value)
+
+cols_to_divide <- c("1", "2", "4", "5", "6", "8", "10")
+
+# convert Ns to percentages
+alteration_counts_cluster <- alteration_counts_cluster %>%
+  mutate(across(all_of(cols_to_divide),
+              ~ .x / cluster_totals[which(cols_to_divide == cur_column())])) %>%
+  write_tsv(file.path(results_dir, "hgg_cluster_mutation_freq.tsv"))
+
