a560034246965b158cd27a2f49a075bd37016289
pj-sullivan <patricia.sullivan199@gmail.com>
2025-06-25 17:49:45 -0400

    comment out error-producing code

--- analyses/oncoprint/run-oncoprint.sh
+++ analyses/oncoprint/run-oncoprint.sh
@@ -4,4 +4,5 @@
 echo "plotting oncoprint"
 Rscript --vanilla 01-oncoprint.R
 
-Rscript --vanilla 02-oncoprint-SFs.R
+#Commenting out as it uses a file not available anymore
+#Rscript --vanilla 02-oncoprint-SFs.R
