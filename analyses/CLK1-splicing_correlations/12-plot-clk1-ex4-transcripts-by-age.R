aafe04ba5027e1abe67dddfe8f124dbd375bf851
pj-sullivan <patricia.sullivan199@gmail.com>
2025-06-27 21:53:20 +0000

    clk1 exon 4 tpm by cluster

--- analyses/CLK1-splicing_correlations/12-plot-clk1-ex4-transcripts-by-age.R
+++ analyses/CLK1-splicing_correlations/12-plot-clk1-ex4-transcripts-by-age.R
@@ -28,6 +28,9 @@ input_dir <- file.path(root_dir, "results")
 # Specify file paths
 clin_file  <- file.path(data_dir,"histologies-plot-group.tsv")
 indep_file <- file.path(data_dir, "independent-specimens.rnaseqpanel.primary.tsv")
+cluster_file <- file.path(root_dir, "analyses",
+                          "sample-psi-clustering", "results",
+                          "sample-cluster-metadata-top-5000-events-stranded.tsv")
 
 # Define file paths
 histology_gtex_file <- file.path(input_dir,"gtex-samples-by-age.tsv")
@@ -43,6 +46,9 @@ source(file.path(root_dir, "figures/theme_for_plots.R"))
 
 # Step 1: Load and filter clinical and independent data
 indep_df <- read_tsv(indep_file)
+cluster_df <- read_tsv(cluster_file) %>%
+  rename(Kids_First_Biospecimen_ID = sample_id) %>%
+  select(Kids_First_Biospecimen_ID, cluster)
 
 hist_indep_rna_df <- read_tsv(clin_file) %>%
   filter(
@@ -64,16 +70,17 @@ all_clk4_transcr_counts <- readRDS(expr_tpm_tumor_file) %>%
   summarise(across(starts_with("BS"), sum, na.rm = TRUE)) %>%
   pivot_longer(cols = -transcript_id, names_to = "Kids_First_Biospecimen_ID", values_to = "TPM") %>%
   inner_join(hist_indep_rna_df, by = "Kids_First_Biospecimen_ID") %>%
+  inner_join(cluster_df, by = "Kids_First_Biospecimen_ID") %>%
   mutate(
     age_years = age_at_diagnosis_days / 365,
     age_bin = cut(
       age_years,
-      breaks = c(0, 14, 18, 39),
-      labels = c("[0-14]", "[15-18]", "[19-39]"),
+      breaks = c(0, 7, 14, 18, 39),
+      labels = c("[0-7]", "[8-14]", "[15-18]", "[19-39]"),
       right = TRUE
     )
   ) %>%
-  select(Kids_First_Biospecimen_ID, plot_group, age_bin, transcript_id, TPM) %>%
+  select(Kids_First_Biospecimen_ID, cluster, plot_group, age_bin, transcript_id, TPM) %>%
   na.omit()
 
 
@@ -102,26 +109,19 @@ transcript_expr_CLK1_combined_df <- transcript_expr_CLK1_combined_df %>%
       TRUE ~ "#b5b5b5"
     )
   ) %>%
-  filter(!is.na(plot_group)) %>%
-  group_by(plot_group) %>%
+  filter(!is.na(cluster)) %>%
+  group_by(cluster) %>%
   mutate(mean_proportion = mean(proportion, na.rm = TRUE)) %>%
-  ungroup() %>%
-  mutate(plot_group = factor(
-    plot_group,
-    levels = transcript_expr_CLK1_combined_df %>%
-      arrange(plot_group, desc(mean_proportion)) %>%
-      pull(plot_group) %>%
-      unique()
-  ))
+  ungroup()
 
 # Step 5: Statistical tests
-plot_p_values_wilcoxon <- data.frame(plot_group = character(), p_value = numeric(), stringsAsFactors = FALSE)
-plot_p_values_permutation <- data.frame(plot_group = character(), p_value = numeric(), stringsAsFactors = FALSE)
+plot_p_values_wilcoxon <- data.frame(cluster = character(), p_value = numeric(), stringsAsFactors = FALSE)
+plot_p_values_permutation <- data.frame(cluster = character(), p_value = numeric(), stringsAsFactors = FALSE)
 
 kruskal_results <- list()
 
-for (group in unique(transcript_expr_CLK1_combined_df$plot_group)) {
-  group_data <- filter(transcript_expr_CLK1_combined_df, plot_group == group)
+for (group in unique(transcript_expr_CLK1_combined_df$cluster)) {
+  group_data <- filter(transcript_expr_CLK1_combined_df, cluster == group)
   
   if (length(unique(group_data$age_bin)) > 1) {
     perm_result <- oneway_test(proportion ~ factor(age_bin), data = group_data, distribution = approximate(nresample = 9999))
@@ -132,8 +132,8 @@ for (group in unique(transcript_expr_CLK1_combined_df$plot_group)) {
       wilcoxon_p_values = wilcox_result$p.value
     )
     
-    plot_p_values_wilcoxon <- rbind(plot_p_values_wilcoxon, data.frame(plot_group = group, p_value = min(wilcox_result$p.value, na.rm = TRUE)))
-    plot_p_values_permutation <- rbind(plot_p_values_permutation, data.frame(plot_group = group, p_value = pvalue(perm_result)))
+    plot_p_values_wilcoxon <- rbind(plot_p_values_wilcoxon, data.frame(cluster = group, p_value = min(wilcox_result$p.value, na.rm = TRUE)))
+    plot_p_values_permutation <- rbind(plot_p_values_permutation, data.frame(cluster = group, p_value = pvalue(perm_result)))
   } else {
     warning(sprintf("Only one group in age_bin for %s", group))
   }
@@ -146,7 +146,7 @@ create_plot <- function(data, title, p_values, value_label) {
     geom_boxplot(aes(group = age_bin), width = 0.6, color = "black", fill = "white", alpha = 0.2) +
     labs(title = title, x = "Age Group", y = "Exon 4 Isoform Fraction") +
     scale_color_identity(name = "Dot Color") +
-    facet_wrap(~plot_group, scales = "free_x", ncol = 5) +
+    facet_wrap(~cluster, scales = "free_x", ncol = 4) +
     theme_Publication() +
     theme(legend.position = "right", axis.text.x = element_text(angle = 75, hjust = 1)) +
     geom_text(
@@ -160,11 +160,11 @@ tpm_plot_wilcoxon <- create_plot(transcript_expr_CLK1_combined_df, "Relative CLK
 tpm_plot_permutation <- create_plot(transcript_expr_CLK1_combined_df, "Relative CLK1 Exon 4 Transcript Expression", plot_p_values_permutation, "")
 
 # Save plots
-pdf(file.path(plots_dir, "clk1ex4-tpm-tumor-age-bin-wc-test.pdf"), height = 14, width = 12)
+pdf(file.path(plots_dir, "clk1ex4-clusters-tpm-tumor-age-bin-wc-test.pdf"), height = 14, width = 12)
 print(tpm_plot_wilcoxon)
 dev.off()
 
-pdf(file.path(plots_dir, "clk1ex4-tpm-tumor-age-bin-perm-test.pdf"), height = 14, width = 12)
+pdf(file.path(plots_dir, "clk1ex4-clusters-tpm-tumor-age-bin-perm-test.pdf"), height = 14, width = 12)
 print(tpm_plot_permutation)
 dev.off()
 
