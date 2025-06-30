f0de983795012a36486f267ecb0dd22a7ac7559f
pj-sullivan <patricia.sullivan199@gmail.com>
2025-06-30 19:17:54 +0000

    rerun module with updated uniprot

--- analyses/splicing_events_functional_sites/03-format_for_ggplot.pl
+++ analyses/splicing_events_functional_sites/03-format_for_ggplot.pl
@@ -13,7 +13,7 @@ print $out_neg "SpliceID\tdPSI\tUniprot\n";
 sub process_file {
     my ($prefix, $suffix, $category, $output) = @_;
 
-    open(my $in, '<', "results/splicing_events.SE.total.$prefix.intersectunip$suffix.hg38.col.wo.txt") or die "Cannot open file: $!";
+    open(my $in, '<', "results/splicing_events.SE.total.$prefix.intersect_UP000005640_9606_$suffix.wo.txt") or die "Cannot open file: $!";
     my %seen;
     while (my $line = <$in>) {
         chomp $line;
@@ -25,16 +25,16 @@ sub process_file {
 }
 
 # Process positive files
-process_file('pos', 'Mod', 'Modifications', $out_pos);
-process_file('pos', 'Other', 'Other', $out_pos);
-process_file('pos', 'DisulfBond', 'DisulfBond', $out_pos);
-process_file('pos', 'LocSignal', 'LocSignal', $out_pos);
+process_file('pos', 'mod_res', 'Modifications', $out_pos);
+process_file('pos', 'domain', 'Domains', $out_pos);
+process_file('pos', 'disulfid', 'DisulfBond', $out_pos);
+process_file('pos', 'signal', 'LocSignal', $out_pos);
 
 # Process negative files
-process_file('neg', 'Mod', 'Modifications', $out_neg);
-process_file('neg', 'Other', 'Other', $out_neg);
-process_file('neg', 'DisulfBond', 'DisulfBond', $out_neg);
-process_file('neg', 'LocSignal', 'LocSignal', $out_neg);
+process_file('neg', 'mod_res', 'Modifications', $out_neg);
+process_file('neg', 'domain', 'Domains', $out_neg);
+process_file('neg', 'disulfid', 'DisulfBond', $out_neg);
+process_file('neg', 'signal', 'LocSignal', $out_neg);
 
 close($out_pos);
 close($out_neg);
