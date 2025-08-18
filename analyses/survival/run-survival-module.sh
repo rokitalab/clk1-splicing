b7fa1990d68ae1f1e4a27bb12732aec01a3d1391
Ryan Corbett <rcorbett@childrensnational.org>
2025-08-18 15:32:56 -0400

    remove outdated scripts, results, plots; re-number retained scripts

--- analyses/survival/run-survival-module.sh
+++ analyses/survival/run-survival-module.sh
@@ -7,16 +7,16 @@ set -o pipefail
 Rscript -e "rmarkdown::render('01-prepare-survival.Rmd')"
 
 # run run-survival scripts
-Rscript -e "rmarkdown::render('02-run-survival-SIgroup.Rmd')"
+#Rscript -e "rmarkdown::render('02-run-survival-SIgroup.Rmd')"
 
 # run plot-survival script
-Rscript --vanilla 03-plot-survival.R
+#Rscript --vanilla 03-plot-survival.R
 
 # run HGG survival by CLK1 status
-Rscript -e "rmarkdown::render('04-survival-hgg-clk1-status.Rmd')"
+#Rscript -e "rmarkdown::render('04-survival-hgg-clk1-status.Rmd')"
 
 # survival analyses by splicing cluster
-Rscript -e "rmarkdown::render('05-survival_by_cluster.Rmd')"
+Rscript -e "rmarkdown::render('02-survival_by_cluster.Rmd')"
 
 # run survival by CLK1 status
 # Rscript -e "rmarkdown::render('06-run-survival-clk1-status-all.Rmd')"
@@ -25,7 +25,7 @@ Rscript -e "rmarkdown::render('05-survival_by_cluster.Rmd')"
 # Rscript --vanilla 07-plot-survival-clk1-status-all.R
 
 # run survival by cluster assignment and SBI or KEGG spliceosome GSVA
-Rscript -e "rmarkdown::render('08-survival_by_cluster_sbi_spliceosome_gsva.Rmd')"
+Rscript -e "rmarkdown::render('03-survival_by_cluster_sbi_spliceosome_gsva.Rmd')"
 
 # Run splicing cluster assignment, splicing burden, gsva, CLK1
-Rscript -e "rmarkdown::render('09-survival_by_cluster_clk1.Rmd')"
+Rscript -e "rmarkdown::render('04-survival_by_cluster_clk1.Rmd')"
